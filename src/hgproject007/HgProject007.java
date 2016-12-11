8d74656fc7333e91b2f68c55219e611cd6214d10
hosein <hgh@siroco.ir>
2016-12-11 22:23:43 +0330

    first step on 3 digit

---
+++ src/hgproject007/HgProject007.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hgproject007;
+
+import java.util.*;
+
+/**
+ *
+ * @author HgH
+ */
+public class HgProject007 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.println(""+tabdil.digit02(5));
+       
+        
+        String result = "";
+        
+        long a ;
+        int numSize;
+      Scanner input = new Scanner(System.in);
+        System.out.println("please enter a number:" );
+      try{
+      a = input.nextLong();
+      
+      }catch(Exception e){
+          System.err.println("please enter a valid number");
+          a = 0;
+      }
+      numSize = tabdil.numSize(a);
+      switch(numSize){
+          case 1: result = tabdil.digit01(a);
+            break;
+          case 2: result = tabdil.digit02(a);
+            break;
+          case 3: result = tabdil.digit03(a);
+            break;
+      }
+        System.out.println("" + result);  
+   
+   }
+}
---
+++ src/hgproject007/tabdil.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hgproject007;
+
+/**
+ *
+ * @author HgH
+ */
+public class tabdil {
+    public static int numSize(long x){ //take a long number and return size of X
+        String str = String.valueOf(x);
+        int a = str.length();
+        
+    return a;
+    }
+    
+    public static String digit01(long x){
+        String result = null;
+        int a = (int) x;
+        switch(a){
+                case(0):result = "صفر";
+                break;
+                case(1):result = "یک";
+                break;
+                case(2):result = "دو";
+                break;
+                case(3):result = "سه";
+                break;
+                case(4):result = "چهار";
+                break;
+                case(5):result = "پنج";
+                break;
+                case(6):result = "شش";
+                break;
+                case(7):result = "هفت";
+                break;
+                case(8):result = "هشت";
+                break;
+                case(9):result = "نه";
+                break;
+        }
+    return result;
+    }
+    
+    public static String digit02(long x){
+        String result = null; //for final return
+        String result1 = null; //for first digit
+        String result2 = null; // for second digit
+        int test = 0 ;
+            if(x<10){result = digit01(x);}
+            if(x<20){
+                    int a = (int) x;
+                    switch(a){
+                    case(10):result = "ده";
+                    break;
+                    case(11):result = "یازده";
+                    break;
+                    case(12):result = "دوازده";
+                    break;
+                    case(13):result = "سیزده";
+                    break;
+                    case(14):result = "چهارده";
+                    break;
+                    case(15):result = "پانزده";
+                    break;
+                    case(16):result = "شانزده";
+                    break;
+                    case(17):result = "هفده";
+                    break;
+                    case(18):result = "هجده";
+                    break;
+                    case(19):result = "نوزده";
+                    break;
+                    }
+            }else{
+                result = String.valueOf(x);
+                result2 = result.substring(0,1);
+                result1 = result.substring(1); // right side sort
+                 test = Integer.parseInt(result1);
+                result1 = digit01(Long.parseLong(result1));
+                
+                switch(result2){
+                        case"2": result2 = "بیست";
+                        break;
+                        case"3": result2 = "سی";
+                        break;
+                        case"4": result2 = "چهل";
+                        break;
+                        case"5": result2 = "پنجاه";
+                        break;
+                        case"6": result2 = "شصت";
+                        break;
+                        case"7": result2 = "هفتاد";
+                        break;
+                        case"8": result2 = "هشتاد";
+                        break;
+                        case"9": result2 = "نود";
+                        break;
+                        
+                }
+                            
+    if(test == 0) {
+        result = result2;
+    
+    }else{
+        result = result2 + " و "+ result1;
+    }   
+            }
+    return result;
+        
+        
+    }
+    public static String digit03(long x){
+    String result = null;
+    String result1 = null;
+    String result2 = null;
+    result = String.valueOf(x);
+    result2 = result.substring(0,1);
+    result1 = result.substring(1,3); // right side sort
+    long test;
+    test = Long.parseLong(result1);
+                switch(result2){
+                    case("1"): result2 = "صد";
+                    break;
+                    case("2"): result2 = "دویست";
+                    break;
+                    case("3"): result2 = "سیصد";
+                    break;
+                    case("4"): result2 = "چهارصد";
+                    break;
+                    case("5"): result2 = "پانصد";
+                    break;
+                    case("6"): result2 = "ششصد";
+                    break;
+                    case("7"): result2 = "هفتصد";
+                    break;
+                    case("8"): result2 = "هشتصد";
+                    break;
+                    case("9"): result2 = "نهصد";
+                    break;
+                }
+    
+    if (!(test == 0)){
+        result1 = digit02(test);
+        result = result2 + " و " + result1;
+    }else{
+        result1 = "";
+        result = result2 ;
+    
+    
+    }
+    
+    
+    return result;
+    }
+}
